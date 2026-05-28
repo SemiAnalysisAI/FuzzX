d443ae447327ce153bd03fb68f1a70683d0da5ae
Justin Lebar <justin.lebar@gmail.com>
2026-05-27 19:37:16 -0700

    x86: remove low-priority NewGVN operand bundle entry

--- x86/bugs/127-newgvn-call-cse-ignores-operand-bundles/cmd.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-OPT=/home/orenamd@semianalysis.com/FuzzX/amdgpu/build/llvm-fuzzer/bin/opt
-echo "===== After newgvn: deopt bundle dropped via CSE ====="
-"$OPT" -passes=newgvn -S repro.ll | grep -E "define|call|add|ret"
--- x86/bugs/127-newgvn-call-cse-ignores-operand-bundles/repro.ll
+++
@@ -1,7 +0,0 @@
-declare i32 @rd(i32) memory(read)
-define i32 @test(i32 %x) {
-  %a = call i32 @rd(i32 %x)
-  %b = call i32 @rd(i32 %x) [ "deopt"() ]
-  %r = add i32 %a, %b
-  ret i32 %r
-}
